9b48b4212973fcdaa7ea3045446224e30f58d57c
envy <silverain2001@gmail.com>
2023-05-10 00:00:11 +0900

    day3-4

---
+++ 3/3-6.py
@@ -0,0 +1,12 @@
+import sys
+list=[]
+number=int(input())
+number<=1000000
+
+for i in range(0,number):
+    A, B= map(int, sys.stdin.readline().split())
+    print(A+B)
+#     list[i]=A+B
+
+# for j in range(0,number):
+#     print(list[i])
