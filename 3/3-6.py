96337a8855df6f3e6cff6e2de063b30e403a46cb
envy <silverain2001@gmail.com>
2023-05-12 02:06:14 +0900

    day4-1

--- 3/3-6.py
+++ 3/3-6.py
@@ -6,7 +6,3 @@ number<=1000000
 for i in range(0,number):
     A, B= map(int, sys.stdin.readline().split())
     print(A+B)
-#     list[i]=A+B
-
-# for j in range(0,number):
-#     print(list[i])
---
+++ 4/4-1.py
@@ -0,0 +1,5 @@
+len=int(input())
+n=list(map(int, input().split()))
+dex=int(input())
+
+print(n.count(dex))
